5620cf88bf0e81c6d8857eaf54828c851cd98a10
AmberShea <ambercroyl@gmail.com>
2024-10-31 15:26:15 -0400

    injected more weather data

--- src/index.js
+++ src/index.js
@@ -2,8 +2,38 @@ function refreshWeather(response) {
 let temperatureElement = document.querySelector("#temperature");
 let temperature = response.data.temperature.current;
 let cityElement = document.querySelector("#city");
+let descriptionElement = document.querySelector("#description");
+let humidityElement = document.querySelector("#humidity");
+let windSpeedElement = document.querySelector("#wind-speed");
+let timeElement = document.querySelector("#time");
+let date = new Date(response.data.time * 1000);
+
 cityElement.innerHTML = response.data.city;
+timeElement.innerHTML = formatDate(date);
+descriptionElement.innerHTML = response.data.condition.description;
+humidityElement.innerHTML = `${response.data.temperature.humidity}%`;
+windSpeedElement.innerHTML = `${response.data.wind.speed}mph`;
 temperatureElement.innerHTML=Math.round(temperature);
+}
+function formatDate(date) {
+let minutes = date.getMinutes();
+let hours = date.getHours();
+let days = [
+    "Sunday", 
+    "Monday", 
+    "Tuesday", 
+    "Wednesday", 
+    "Thursday", 
+    "Friday", 
+    "Saturday"
+];
+let day = days[date.getDay()];
+
+    if (minutes< 10) {
+        minutes=`0${minutes}`;
+    }
+
+    return `${day} ${hours}:${minutes}`;
 }
 function searchCity(city) {
 let apiKey = "07b4o18620b3fb3dt57a04f9330d1c11";
@@ -14,7 +44,7 @@ axios.get(apiUrl).then(refreshWeather);
 function handleSearchSubmit(event) {
     event.preventDefault();
 let searchInput=document.querySelector("#search-form-input");
-let cityElement=document.querySelector("#city");
+let cityElement = document.querySelector("#city");
 cityElement.innerHTML = searchInput.value;
 searchCity(searchInput.value);
 }
