faeeb4f3caaf14e12b736004d1e250c7e7d33a7d
AmberShea <ambercroyl@gmail.com>
2024-11-06 22:45:47 -0500

    adjusted css and replaced dummy info with api

--- src/index.js
+++ src/index.js
@@ -56,6 +56,11 @@ let searchFormElement = document.querySelector("#search-form");
 searchFormElement.addEventListener("submit", handleSearchSubmit);
 searchCity("Paris");
 
+function formatDay(timestamp) {
+    let date =new Date(timestamp * 1000);
+    let days =["Sun", "Mon", "Tue", "Wed", "Thu", "Fri", "Sat"];
+    return days[date.getDay()];
+}
 function getForecast(city){
     let apiKey = "07b4o18620b3fb3dt57a04f9330d1c11";
     let apiUrl = `https://api.shecodes.io/weather/v1/forecast?query=${city}&key=${apiKey}&units=imperial`;
@@ -65,28 +70,30 @@ function getForecast(city){
 function displayForecast(response) {
     console.log(response.data);
 
-
-let days = ["Tue", "Wed", "Thu", "Fri", "Sat"];
 let forecastHTML = "";
-days.forEach(function (day){
 
+response.data.daily.forEach(function (day, index){
+if (index < 5) {
 forecastHTML =
 forecastHTML + 
 `
 <div class="weather-forecast-day">
-    <div class="weather-forecast-date">${day}</div>
-    <div class="weather-forecast-icon">⛅</div>
+    <div class="weather-forecast-date">${formatDay(day.time)}</div>
+    <img src="${day.condition.icon_url}" class="weather-forecast-icon"/>
     <div class="weather-forecast-temperatures">
     <div class="weather-forecast-temperature">
-    <strong>15&deg</strong>
+    <strong>${Math.round(day.temperature.maximum)}&deg</strong>
     </div>
-    <div class="weather-forecast-temperature"> 9&deg</div>
+    <div class="weather-forecast-temperature">${Math.round(day.temperature.minimum)}&deg</div>
     </div>
 </div>
 `;
+}
 });
+
 let forecast = document.querySelector("#forecast");
 forecast.innerHTML = forecastHTML
 }
+
 displayForecast("Paris");
 
